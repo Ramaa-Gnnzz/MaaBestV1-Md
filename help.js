42db5a9c05ca83431621632e1ea835267394fc9e
Ramaa-Gnnzz <rhamadhan01@gmail.com>
2023-01-01 20:35:25 +0700

    bebas

---
+++ help.js
@@ -0,0 +1,61 @@
+
+// disini atur aja kak menu bot nya
+// jangan lupa hapus fitur yg ga penting
+
+var monoSpace = '```'
+
+
+exports.rulesBot = () =>{
+return`*──「 RULES-BOT 」──*
+
+1. Dilarang spam bot✉️. 
+Sanksi: *BLOCK*
+
+2. Dilarang Call bot📞.
+Sanksi: *BLOCK*
+
+3. Jangan Mengina bot📣.
+Sanksi: *PERMANENT BLOCK*
+
+Mohon Di pahami atas Peraturan penggunaan bot WhatsApp.`
+}
+
+exports.donasiBot = (cekName,ucapanWaktu) =>{
+return`──「 *MENU DONATE* 」──
+
+Hi *${cekName}* ${ucapanWaktu} 👋🏻
+
+*Payment Ovo*
+Number: 085806240904
+
+*Payment Dana*
+Number: 085806240904
+
+*Payment Gopay*
+Number: ----
+
+Satu sedekah yang tulus sama dengan seribu langkah menuju surga.
+صدقة واحدة مخلصة تساوي ألف خطوة إلى الجنة.
+
+${monoSpace} berapapun donasi dari kalian itu sangat berarti bagi kami ^_^${monoSpace}
+──「 *THX FOR YOU* 」──`
+}
+
+exports.infoOwner = () =>{
+return`──「 *DATA PROFIL OWNER* 」──
+
+ ⬣ *Nama :* Rama 
+ ⬣ *Umur :* Privv
+ ⬣ *Hoby. :* Turu/Game
+ ⬣ *Asal   :* Jatim - Lamongan
+ ⬣ *Status:* Creator
+ ⬣ *Zodiak:* Gk tau
+
+                  「 *SOSIAL MEDIA*」
+ ⬣ *Whatsapp:* 085791220179
+ ⬣ *Youtube:* Ramaa Gnnz
+ ⬣ *Github:* ---
+ ⬣ *Tiktok:* ---
+ ⬣ *Instagram:* ---
+ `
+}
